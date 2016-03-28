bbca5fb602111ba0b73a6b4bcf39572094320b50
SeanWong933 <siegrainwong@gmail.com>
2016-03-28 14:51:06 +0800

    Initial Commit

---
+++ WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by Siegrain on 16/3/28.
+//  Copyright © 2016年 siegrain. weChat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
